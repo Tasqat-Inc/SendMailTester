9680641c3f7c39755e02f3b2319b3db011c3554a
Ahmad Alkhawaja <elkhawaja@gmail.com>
2020-02-02 13:26:20 +0400

    Initial commit

---
+++ SendMail/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using SendMail.ViewModel;
+
+namespace SendMail.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SendEmail(EmailSettings obj)
+        {
+            string result = "";
+            try
+            {
+                MailMessage mail = new MailMessage(obj.From, obj.To);
+                SmtpClient client = new SmtpClient();
+                client.UseDefaultCredentials = false;
+                client.Port = obj.SmtpPort;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                client.Host = obj.SmtpHost;
+                client.Credentials = new System.Net.NetworkCredential(obj.Username, obj.Password);
+                client.EnableSsl = obj.SSL;
+
+                mail.Subject = obj.Subject;
+                mail.Body = obj.Body;
+                mail.IsBodyHtml = true;
+                mail.BodyEncoding = UTF8Encoding.UTF8;
+                mail.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+
+
+                client.Send(mail);
+                result = "1";
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null)
+                {
+                    result = ex.Message + " - " + ex.InnerException.Message;
+                }
+                else
+                {
+                    result = ex.Message;
+                }
+            }
+
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ SendMail/ViewModel/EmailSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SendMail.ViewModel
+{
+    public class EmailSettings
+    {
+        public string  SmtpHost { get; set; }
+        public int  SmtpPort { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool SSL { get; set; }
+    }
+}
